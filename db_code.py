1b02ebff5b7a9cbb4371e38ae0096f6056d240ec
Jumponpatha Chaimongkonrojna <91845870+Jumponpatha@users.noreply.github.com>
2024-10-30 01:10:20 +0700

    Update db_code.py

--- db_code.py
+++ db_code.py
@@ -15,7 +15,7 @@ tables = data.find_all('tbody')
 rows = tables[0].find_all('tr')
 
 for row in rows:
-    if count < 25:
+    if count < 50:
         col = row.find_all('td')
         if len(col) != 0:
             data_dict = {
